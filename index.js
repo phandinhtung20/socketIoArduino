2611091baae3201d2cffb3cb69b0750e81a49506
Phan Đình Tùng <Phan nh Tng>
2017-09-30 09:42:35 +0700

    socket io on may arduino

---
+++ index.js
@@ -0,0 +1,28 @@
+var express= require('express'),
+    app= express(),
+    port= 3000 || env.process.PORT,
+    http=require('http'),
+    server= http.createServer(app),
+    io= require('socket.io')(server);
+server.listen(port,'192.168.0.105',function(){
+  console.log('On server');
+});
+app.use(express.static('publics'));
+app.set('view engine','ejs');
+app.set('views','./views');
+io.sockets.on('connection',function(socket){
+  ids=socket.id;
+  console.log('On Socket',ids);
+  socket.emit('hello',{id: socket.id});
+  socket.on('den', function (data) {
+    console.log(data);
+    if (data.data == '1') io.sockets.emit('change',{data: '1'});
+    if (data.data == '0') io.sockets.emit('change',{data: '0'});
+  });
+});
+app.get("/check", function (req,res) {
+  res.send("hello Tung");
+});
+app.get("/", function (req,res) {
+  res.render("socket");
+});
